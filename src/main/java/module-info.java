bb79351068efb7ba9d980652acfaa7e125cd7132
Adam Bogusz <pb3k.84+git@gmail.com>
2025-02-13 22:47:37 +0100

    Zmiana na niemodularny projekt

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module pl.boguszadam.backupapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens pl.boguszadam.backupapp to javafx.fxml;
-    exports pl.boguszadam.backupapp;
-}
---
+++ src/main/java/pl/boguszadam/backupapp/Launcher.java
@@ -0,0 +1,7 @@
+package pl.boguszadam.backupapp;
+
+public class Launcher {
+    public static void main(String[] args) {
+        BackupApplication.main(args);
+    }
+}
